b9dcdb089299c9ff69bc5c5f46a4967e82104dba
Jairo García <jairo.garcia@ceu.es>
2022-10-19 19:27:45 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<strong>Hola</strong>
+<?php echo "Jairo"; ?>
+<br>
+<?php
+    //Comentario de una línea
+    /*
+     * Comentario multilínea
+     */
+    //Si escribo todo entiende que es algo por hacer
+    $nombre = "Jairo";
+    $numero = 7;
+    echo "Mi nombre es ".$nombre." y mi número es ".$numero;
+?>
+<p>
+    Mi nombre es <?php echo $nombre ?> y mi número es <?php echo $numero ?>
+</p>
