4164d70dac4a69b052ecd0963435353dbd66d48d
Jairo García <jairo.garcia@ceu.es>
2022-10-19 19:53:22 +0200

    Estructura inicial

---
+++ ejercicio-1-pares-o-nones/index.php
@@ -0,0 +1,19 @@
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Ejercicio propuesto 1</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <h1>Ejercicio propuesto 1</h1>
+        <p>
+            Desarrolla un juego web para jugar a pares o nones contra el ordenador con las siguientes especificaciones:
+        </p>
+        <ol>
+            <li>Tú serás pares y debes poder elegir del 1 al 5 en cada tirada</li>
+            <li>La máquina será nones y elegirá un número del 1 al 5 en cada tirada</li>
+            <li>Un texto debe mostrar el número que tú elegiste, el que eligió la máquina y quién ha ganado.</li>
+            <li>PLUS: Modifica ahora el juego para que el usuario pueda elegir si es pares o nones.</li>
+        </ol>
+    </body>
+</html>
